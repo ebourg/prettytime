27243bdaff55d28edcb9dac3ad584e39db33ab93
Emmanuel Bourg <ebourg@apache.org>
2026-01-29 14:45:52 +0100

    Fix parameter documentation in TimeFormat.java

--- core/src/main/java/org/ocpsoft/prettytime/TimeFormat.java
+++ core/src/main/java/org/ocpsoft/prettytime/TimeFormat.java
@@ -25,14 +25,14 @@ public interface TimeFormat
    /**
     * Given a populated {@link Duration} object. Apply formatting (with rounding) and output the result.
     * 
-    * @param The original {@link Duration} instance from which the time string should be decorated.
+    * @param duration The original {@link Duration} instance from which the time string should be decorated.
     */
    public abstract String format(final Duration duration);
 
    /**
     * Given a populated {@link Duration} object. Apply formatting (without rounding) and output the result.
     * 
-    * @param The original {@link Duration} instance from which the time string should be decorated.
+    * @param duration The original {@link Duration} instance from which the time string should be decorated.
     */
    public String formatUnrounded(Duration duration);
 
